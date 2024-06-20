26155874b5efe17ab8c719768bc7a036e28e3737
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-21 01:51:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@
 resource "aws_instance" "strapi_instance" {
   ami             = "ami-003c463c8207b4dfa"  // Update with your Ubuntu 20.04 AMI ID
   instance_type   = "t2.medium"
+  subnet_id       = "subnet-0ba5354c77b6d001f"  // Update with your subnet ID
   key_name        = "TASK2"  // Replace with your key pair name
-
   subnet_id       = "subnet-0ba5354c77b6d001f"  // Update with your subnet ID
   vpc_security_group_ids = ["sg-0795d88eba4e1b125"]  // Update with your security group ID
 
