9d5d247288b54019001f6ad235e0f3924a123408
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-21 02:02:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
- provider "aws" {
+provider "aws" {
   region = "ap-southeast-1"
 }
 
-
 # Define the EC2 instance
 resource "aws_instance" "strapi_instance" {
-  ami             = "ami-003c463c8207b4dfa"  // Update with your Ubuntu 20.04 AMI ID
-  instance_type   = "t2.medium"
-  subnet_id       = "subnet-0ba5354c77b6d001f"  // Update with your subnet ID
-  key_name        = "TASK2"  // Replace with your key pair name
+  ami                    = "ami-003c463c8207b4dfa"  // Update with your Ubuntu 20.04 AMI ID
+  instance_type          = "t2.medium"
+  subnet_id              = "subnet-0ba5354c77b6d001f"  // Update with your subnet ID
+  key_name               = "TASK2"  // Replace with your key pair name
   vpc_security_group_ids = ["sg-0795d88eba4e1b125"]  // Update with your security group ID
 
   tags = {
@@ -20,8 +19,9 @@ resource "aws_instance" "strapi_instance" {
     user        = "ubuntu"
     private_key = file("/home/ubuntu/TASK2.pem")  // Update with correct path to your .pem file
     host        = "13.229.243.5"
- }
+  }
 }
+
 output "instance_public_ip" {
   value = aws_instance.strapi_instance.public_ip
 }
