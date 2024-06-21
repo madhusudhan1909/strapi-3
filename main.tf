c0dffd96a603aad5720544db6b8d3438f5738162
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-21 08:12:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# Define the EC2 instance
 resource "aws_instance" "strapi_instance" {
-  ami                    = "ami-003c463c8207b4dfa"  // Update with your Ubuntu 20.04 AMI ID
+  ami                    = "ami-003c463c8207b4dfa"  # Update with your Ubuntu 20.04 AMI ID
   instance_type          = "t2.medium"
-  subnet_id              = "subnet-0ba5354c77b6d001f"  // Update with your subnet ID
-  key_name               = "TASK2"  // Replace with your key pair name
-  vpc_security_group_ids = ["sg-0795d88eba4e1b125"]  // Update with your security group ID
+  subnet_id              = "subnet-0ba5354c77b6d001f"  # Update with your subnet ID
+  key_name               = "TASK2"  # Replace with your key pair name
+  vpc_security_group_ids = ["sg-0795d88eba4e1b125"]  # Update with your security group ID
 
   tags = {
     Name = "srv-strapi"
@@ -17,8 +16,20 @@ resource "aws_instance" "strapi_instance" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("/home/ubuntu/TASK2.pem")  // Update with correct path to your .pem file
-    host        = "13.229.243.5"
+    private_key = file("/home/ubuntu/TASK2.pem")  # Update with correct path to your .pem file
+    host        = aws_instance.strapi_instance.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update -y",
+      "sudo apt-get install -y nodejs npm",
+      "sudo npm install -g pm2",
+      "git clone https://github.com/PearlThoughts-DevOps-Internship/strapi.git /srv/strapi",
+      "cd /srv/strapi/examples/getstarted",
+      "npm install",
+      "pm2 start npm -- start"
+    ]
   }
 }
 
