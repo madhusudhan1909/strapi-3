bd2a13874dbc5f2f734b7478afc893c119f3be72
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-21 08:27:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 provider "aws" {
   region = "ap-southeast-1"
 }
-
-variable "private_key" {
-  description = "The private key for SSH access"
-  type        = string
-  sensitive   = true
-}
-
 resource "aws_instance" "strapi_instance" {
   ami                    = "ami-003c463c8207b4dfa"  # Update with your Ubuntu 20.04 AMI ID
   instance_type          = "t2.medium"
